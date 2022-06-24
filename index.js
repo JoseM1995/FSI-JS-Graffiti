fd96ceec5093ec35df7c4f83f3814888aeeb7d70
Jose Montoya <josem@Joses-MacBook-Pro.local>
2022-06-23 19:15:32 -0700

    new

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let header = document.querySelector('#page-header')
+header.style.color = "green"
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '35px'
+    dogImages[i].style.flip 
+
+}
+ 
+footer.style.borderStyle = 'dotted'
+footer.style.color = 'green'
+
+
 
 // Select some elements...
